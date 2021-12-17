20d5aab4db6b0e2deb314d589a1c31626d1db2d7
Shenmew <Cessnator007@gmail.com>
2021-12-16 21:15:52 -0800

    library scripts and design

--- src/library/interface.js
+++ src/library/interface.js
@@ -1,4 +1,4 @@
-import { testCategoryArray, testBookArray } from "./script.js";
+import { testCategoryArray, testBookArray, categoryManager } from "./script.js";
 import style from "./style.css"
 
 let background = document.createElement("div");
@@ -7,17 +7,22 @@ background.setAttribute('class','background');
 let container = document.createElement("div");
 container.setAttribute('id', 'library-container')
 background.appendChild(container);
-
 //uses globally scopped CSS declarations? Wtf? Why?
+
+
+//BASIC LAYOUT
+
 function createDomElement(element){
     let temp = document.createElement(element.type || 'div');
     if(element.id){temp.setAttribute('id', element.id)};
     if(element.class){temp.classList.add(element.class)};
+    if(element.object){temp.object = element.object};
     temp.textContent = element.text;
     element['appendTo'].appendChild(temp);
     return temp;
 }
 
+//TOP BANNER
 let banner = createDomElement({
     id: "banner",
     appendTo: container
@@ -31,28 +36,82 @@ let banner = createDomElement({
             text: "CATEGORIES",
             appendTo: categories
         });
+        //ADD CATEGORY
         createDomElement({
             id: "add-category",
-            text: "Add New Category",
+            text: "ADD NEW CATEGORY",
             appendTo: categories
-        });
+            }).onclick = () => {
+                let categoryPrompt = prompt("What category would you like to add?", "category");
+                console.log(categoryPrompt);
+                categoryManager.addCategory(categoryPrompt);
+                addCategoryCard(categoryPrompt);
+            }
+    //CATEGORY CONTAINER
     let categorySelect = createDomElement({
         id: "category-select",
         appendTo: banner,
     })
-        //pull categories from script and display, and this one is only text, but the real one should have an object with an associated operation
-        testCategoryArray.forEach((test) => {
+        //ADDS BOOKS
+        function addBook(text){
+            let elly = createDomElement({
+                class: 'book',
+                appendTo: contentMargin,
+                text: text
+            });
+        }
+        //SWITCH CONTENT
+        let contentSwitch = ((category) => {
+            let object = category.target.object
+            while(contentMargin.childElementCount > 0){
+                contentMargin.removeChild(contentMargin.firstElementChild);
+            }
+            createDomElement({
+                class: 'book',
+                appendTo: contentMargin,
+                text: "ADD BOOK"
+            }).onclick = () => {
+                let booktitle = prompt("what is the books title?", 'booktitle');
+                console.log(booktitle);
+                if(booktitle){
+                    categoryManager.addBook(object, booktitle)
+                    addBook(booktitle);
+                };
+               // console.log(categoryManager.addBook(category, booktitle));
+            }
+            if(typeof object.books !== 'undefined'){
+                object.books.forEach((book) => {
+                addBook(book)
+                })
+            }
+        }); 
+        categoryManager.categories.forEach((category) => {
             createDomElement({
                 class: "category",
                 appendTo: categorySelect,
-                text: test
+                text: category.categoryname,
+                object: category
+            //ON CLICK FOR EACH CATEGORY TO CHANGE BOOKS
+            }).onclick = contentSwitch;
+               
+        });
+        //ADD BOOKS -- END
+
+        function addCategoryCard(textcontent){
+            createDomElement({
+                class: "category",
+                appendTo: categorySelect,
+                text: textcontent
             })
-        })
+        }
     createDomElement({
         id: "return-to-dash",
         appendTo: banner,
-        text: "Return To Dashboard"
+        text: "RETURN TO DASHBOARD"
     })    
+//END TOP BANNER
+
+//CONTENT/BOOKS
 let content = createDomElement({
     id: "content",
     appendTo: container
@@ -67,10 +126,36 @@ let content = createDomElement({
             appendTo: contentMargin,
             text: book
         })
-    })
+})
+    
+//Category Select Drag
+let pos = { top: 0, left: 0, x: 0, y: 0 };
+const mouseDownHandler = function (e) {
+    categorySelect.style.cursor = 'grabbing';
+    categorySelect.style.userSelect = 'none';
+    pos = {
+        // The current scroll
+        left: categorySelect.scrollLeft,
+        // Get the current mouse position
+        x: e.clientX,
+    };
 
-
-
+    categorySelect.addEventListener('mousemove', mouseMoveHandler);
+    categorySelect.addEventListener('mouseup', mouseUpHandler);
+};
+const mouseMoveHandler = function (e) {
+    const dx = e.clientX - pos.x;
+    categorySelect.scrollLeft = pos.left - dx;
+};
+const mouseUpHandler = function () {
+    categorySelect.removeEventListener('mousemove', mouseMoveHandler);
+    categorySelect.removeEventListener('mouseup', mouseUpHandler);
+    categorySelect.style.cursor = 'pointer';
+    categorySelect.style.removeProperty('user-select');
+};
+categorySelect.onmousedown = mouseDownHandler;
+categorySelect.onmouseleave = mouseUpHandler;
+categorySelect.onmouseup = mouseUpHandler;
 
 
 export default background;
--- src/library/script.js
+++ src/library/script.js
@@ -1,9 +1,5 @@
 //Going to run everything in here with IIFE, could be split?
-
-const DOM = (function(){
-    const libraryInterface = document.createElement('div').classList.add('interface');
-
-})();
+const myStorage = window.localStorage;
 
 const bookLibrary = (function(){
     books = new Array();
@@ -40,24 +36,49 @@ const libraryStorage = (function(){
         changeTitle
     }
 })();
-const jsonStorage = (function(){
-    if(localStorage.length>0){
-        const myStorage = JSON.parse(localStorage);
-    }else{
-        const myStorage = new Array();
-    }
-    function addBookToStorage(book){
-        myStorage.push(book);
-        localStorage = JSON.stringify(myStorage);
-    }
-    return addBookToStorage;
-})();
+// const jsonStorage = (function(){
+//     if(localStorage.length>0){
+//         myStorage = JSON.parse(localStorage);
+//     }else{
+//         const myStorage = new Array();
+//     }
+//     function addBookToStorage(book){
+//         myStorage.push(book);
+//         localStorage = JSON.stringify(myStorage);
+//     }
+//     return addBookToStorage;
+// })();
 
+const categoryManager = (function(){
+    function Category(categoryname){
+        this.categoryname = categoryname,
+        this.books = []
+        //I KNOW THIS IS REALLY FUCKING LAZY, but fuck it, for now...
+    }
+    function addBook(category, bookname){
+        category.books.push(bookname)
+        myStorage.categories = JSON.stringify(categories);
+    }
+    console.log(typeof myStorage.categories === 'undefined')
+    let categories = [];
+    //possible naming issue with category being both the local name and class name?
+    categories.forEach((category) => console.log(category.protoype));
+    if(typeof myStorage.categories !== 'undefined'){
+        categories = JSON.parse(myStorage.categories);
+        //categories.forEach((category) => Object.setPrototypeOf(category, Category))
+    }
+    console.log(categories);
+    function addCategory(categoryname){
+        categories.push(new Category(categoryname));
+        myStorage.categories = JSON.stringify(categories);   
+    }
+    return {addCategory, categories, addBook}
+})();
 // const thalamus = (function(){
 
 // })();
 let testCategoryArray = ["Current Reads", "Example 1", "Example 2", "Example 3", "Example 4"]
 let testBookArray = ["Book 1", "Book 2", "Book 3", "Book 4", "Book 5", "Book 6", "Book 7", "Book 8", "Book 9", "Book 10"]
-export {libraryStorage, jsonStorage, testCategoryArray, testBookArray}
+export {libraryStorage, myStorage, testCategoryArray, testBookArray, categoryManager}
 
 
