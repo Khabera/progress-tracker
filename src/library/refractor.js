b27fd38115619e56488b0edb126ecb059add590f
Shenmew <Cessnator007@gmail.com>
2021-12-24 13:39:04 -0800

    refactor with mainlayout

--- src/library/refractor.js
+++ src/library/refractor.js
@@ -2,6 +2,7 @@ import { testCategoryArray, testBookArray, categoryManager } from "./script.js";
 import style from "./style.css"
 
 //STANDARD FUNCTION TO CREATE DOM ELEMENTS
+//REPLACE "createDomElement" with cDM
 function createDomElement(element){
     let temp = document.createElement(element.type || 'div');
     if(element.id){temp.setAttribute('id', element.id)};
@@ -24,10 +25,25 @@ const DOM = (function(){
         let background = createDomElement({class: 'background'});
         let firstContainer = createDomElement({id: 'library-container', appendTo: background});
         let topBanner = createDomElement({id: 'banner', appendTo: firstContainer});
-        let categories = createDomElement({id: ''})
-
+            let leftBannerDiv = createDomElement({id: 'left-banner-div', appendTo: topBanner});
+                let categoryTitle = createDomElement({id: "category-title", text: "CATEGORIES", appendTo: leftBannerDiv});
+                let addCategoryButton = createDomElement({id: "add-category", text: "ADD NEW CATEGORY", appendTo: leftBannerDiv});
+                let deleteCategoryButton = createDomElement({id: "delete-category", text: "REMOVE CATEGORY", appendTo: leftBannerDiv});
+            let categoriesDiv = createDomElement({id: "category-select", appendTo: topBanner});
+            let returnToDash = createDomElement({id: "return-to-dash", appendTo: topBanner, text: "RETURN TO DASHBOARD"});
+        let libraryBackground = createDomElement({id: "library-background", appendTo: firstContainer})
+        let libraryContentDiv = createDomElement({id: "library-content", appendTo: libraryBackground});
         return {
-            background
+            background, 
+            addCategoryButton, deleteCategoryButton, returnToDash,
+            categoriesDiv, libraryContentDiv
+        }
+    })();
+    const CATEGORIES = (function(){
+        let categoriesDiv = MAINLAYOUT.categoriesDiv;
+        
+        return {
+
         }
     })();
     return {
