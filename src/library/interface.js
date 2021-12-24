e376dfcf4a886a22a31c699e094e2d2b2248d455
Shenmew <Cessnator007@gmail.com>
2021-12-23 16:51:37 -0800

    table of contents functionality

--- src/library/interface.js
+++ src/library/interface.js
@@ -27,6 +27,7 @@ let banner = createDomElement({
     id: "banner",
     appendTo: container
 })
+//CATEGORY CONTAINER AND MANAGEMENT FUNCTIONS
     let categories = createDomElement({
         id: "categories",
         appendTo: banner
@@ -58,7 +59,6 @@ let banner = createDomElement({
             appendTo: categories
         }).onclick = () => {
             let categories = document.getElementsByClassName('category');
-            console.log(categories);
             //REMOVES DELETE OPTIONS FROM CATEGORIES ON INTERFACE
             let removeDelete = () => {
                 console.log('remove function');
@@ -87,33 +87,114 @@ let banner = createDomElement({
         //ADDS BOOKS
         //ADD BOOK FUNCTION
         function addBook(bookCategory, book){
-            console.log(book);
             let elly = createDomElement({
                 class: 'book',
                 appendTo: contentMargin,
             });
-                createDomElement({
+                let title = createDomElement({
                     class: 'title',
                     appendTo: elly,
                     text: book.booktitle
                 })
-                createDomElement({
+                let subtitle = createDomElement({
                     class: 'subtitle',
                     appendTo: elly,
                     text: book.subtitle
                 })
                 elly.bookAttatchment = book;
+                //BUTTONS DIV
+                let buttonsDiv = createDomElement({
+                    class: 'book-buttons-div',
+                    appendTo: elly
+                })
+                //DELETE BUTTON
                 let deleteButton = createDomElement({
                     class: 'delete',
-                    appendTo: elly,
-                    text: 'D'
+                    appendTo: buttonsDiv,
+                    text: 'delete'
                 })
                 deleteButton.onclick = () => {
                     if(confirm('Are you sure you would like to delete?') == true){
                         contentMargin.removeChild(elly);
                         categoryManager.removeBook(bookCategory, book);
                     }    
-                }    
+                }
+                //COMPLETE BUTTON
+                let completeButton = createDomElement({
+                    class: 'complete',
+                    appendTo: buttonsDiv,
+                    text: 'complete'
+                })
+                completeButton.onclick = () => {
+                    book.completed = !book.completed;
+                    if(book.completed){
+                        title.style.color = 'green';
+                    }else{
+                        title.style.color = 'black';
+                    }
+                    
+                }
+                //CONTENTS MANAGER
+                let expandContainer = createDomElement({
+                    class: 'expand-button',
+                    appendTo: elly,
+                    text: '⤈⤈ TABLE OF CONTENTS ⤈⤈'
+                })
+                expandContainer.onclick = () => {
+                    console.log(contentContainer)
+                    if(contentContainer.style.height == 'auto'){
+                        console.log('greater')
+                        contentContainer.style.height = '0';
+                        contentContainer.style.overflow = 'hidden';
+                    }else{
+                        console.log('else')
+                        contentContainer.style.height = 'auto';
+                        contentContainer.style.overflow = 'scroll';
+                    }
+                }
+                let contentContainer = createDomElement({
+                    class: 'booktablecontents',
+                    appendTo: elly
+                })
+                //CONTENT ADDITIONS
+                let addContent = (click) => {
+                    console.log(click.target)
+                    //CHOOSE APPEND TARGET (SO APPENDS CORRECTLY FOR BOTH TOP AND CONTENT ADD BUTTONS)
+                    let appendTarget;
+                    if(click.target.parentElement.classList.contains('top-content')){
+                        appendTarget = click.target.parentElement.parentElement
+                        console.log('true');
+                    }else{
+                        appendTarget = click.target.parentElement
+                        console.log(click.target.parentElement);
+                    }
+                    let fullContainer = createDomElement({
+                        class: 'content',
+                        appendTo: appendTarget
+                    })
+                    let container = createDomElement({
+                        class: 'top-content',
+                        appendTo: fullContainer
+                    })
+                    content = createDomElement({
+                        appendTo: container,
+                        text: prompt('what is the text')
+                    })
+                    let subAdd = createDomElement({
+                        appendTo: container,
+                        appendTarget: fullContainer,
+                        text: 'add'
+                    })
+                    subAdd.onclick = addContent;
+                }
+                    //TOP ADD BUTTON
+                    let addButton = createDomElement({
+                        class: 'content-add-button',
+                        appendTo: contentContainer,
+                        text: 'add'
+                    });
+                    addButton.onclick = addContent;
+                
         }
         //END ADD BOOK FUNCTION
         //SWITCH CONTENT
@@ -133,8 +214,8 @@ let banner = createDomElement({
                 let book = {
                     booktitle: prompt("what is the books title?", 'booktitle'),
                     subtitle: prompt("what is the books subtitle", 'subtitle'),
+                    completed: false
                 }
-                console.log(book);
                 if(book.booktitle){
                     categoryManager.addBook(bookCategory, book)
                     addBook(bookCategory, book);
@@ -146,7 +227,6 @@ let banner = createDomElement({
             //GET BOOKS ON LOAD
             if(typeof bookCategory.books !== 'undefined'){
                 bookCategory.books.forEach((book) => {
-                console.log(book);
                 addBook(bookCategory, book)
                 })
             }
@@ -162,7 +242,7 @@ let banner = createDomElement({
             temp.onclick = contentSwitch;
         });
         //ADD BOOKS -- END
-
+        //CATEGORY CARD
         function addCategoryCard(textcontent){
             createDomElement({
                 class: "category",
@@ -170,6 +250,7 @@ let banner = createDomElement({
                 text: textcontent
             })
         }
+    //RETURN TO DASHBOARD
     createDomElement({
         id: "return-to-dash",
         appendTo: banner,
@@ -178,6 +259,7 @@ let banner = createDomElement({
 //END TOP BANNER
 
 //CONTENT/BOOKS
+//CONTENT CONTAINERS
 let content = createDomElement({
     id: "content",
     appendTo: container
@@ -186,6 +268,7 @@ let content = createDomElement({
         id: "inner-content",
         appendTo: content
     })
+    //ADD BOOKS ON LOAD
     testBookArray.forEach((book) => {
         createDomElement({
             class: 'book',
